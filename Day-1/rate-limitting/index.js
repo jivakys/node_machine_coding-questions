552bc2c6ab8352f525e6a65fbe51ae81136914be
Jivak Sute <jivakysute@gmail.com>
2025-12-16 12:04:02 +0530

    rate limitter

--- Day-1/rate-limitting/index.js
+++ Day-1/rate-limitting/index.js
@@ -3,8 +3,9 @@ const apiLimiter = require("./middleware");
 
 const app = express();
 app.use(express.json());
+app.use(apiLimiter);
 
-app.get("/api/data", apiLimiter, (req, res) => {
+app.get("/api/data", (req, res) => {
   res.json({
     success: true,
     message: "Data fetched successfully",
