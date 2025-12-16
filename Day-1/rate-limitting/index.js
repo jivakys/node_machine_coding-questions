efffa5aa1ac075cb4c37787bdbad3edc21ab0cd5
Jivak Sute <jivakysute@gmail.com>
2025-12-16 12:02:15 +0530

    rate limitter

--- Day-1/rate-limitting/index.js
+++ Day-1/rate-limitting/index.js
@@ -1,20 +1,7 @@
 const express = require("express");
-const rateLimitter = require("express-rate-limit");
 
 const app = express();
 app.use(express.json());
-
-const apiLimiter = rateLimitter({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-  message: {
-    success: false,
-    message: "Too many requests from this IP, please try again after 1 hour",
-  },
-  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
-});
 
 app.get("/api/data", apiLimiter, (req, res) => {
   res.json({
---
+++ Day-1/rate-limitting/middleware.js
@@ -0,0 +1,15 @@
+const rateLimitter = require("express-rate-limit");
+
+const apiLimiter = rateLimitter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  message: {
+    success: false,
+    message: "Too many requests from this IP, please try again after 1 hour",
+  },
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+});
+
+module.exports = apiLimiter;
