0c7117e00be73d0653c83e82b4a3ea26c21fcfe9
Jivak Sute <jivakysute@gmail.com>
2025-12-18 12:34:59 +0530

    Error handling in express application

--- Day-1/error-handling/index.js
+++ Day-1/error-handling/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const userRouter = require("./routes/userRouter");
+const NotFoundError = require("./errors/notFoundError");
+const errorHandler = require("./middleware/errorHandler");
 const app = express();
 require("dotenv").config();
 
@@ -7,6 +10,15 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Error Handling app");
 });
+app.use("/users", userRouter);
+
+// Handle undefined routes
+app.use((req, res, next) => {
+  next(new NotFoundError("Route not found"));
+});
+
+// Global error handler (ALWAYS LAST)
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
   console.log(`server running on port ${process.env.PORT}`);
--- Day-1/error-handling/routes/userRouter.js
+++ Day-1/error-handling/routes/userRouter.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const validationError = require("../errors/validationError");
+const NotFoundError = require("../errors/notFoundError");
 
 const userRouter = express.Router();
 
-userRouter.get("/", (req, res) => {});
+userRouter.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return next(new validationError("User ID is required"));
+  }
+
+  const user = null;
+
+  if (!user) {
+    return next(new NotFoundError("User not found"));
+  }
+
+  res.json({ success: true, data: user });
+});
 
 module.exports = userRouter;
