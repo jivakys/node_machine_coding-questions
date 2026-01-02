743016bc56c17d0667283c27dbd0944f3ecd32bd
Jivak Sute <jivakysute@gmail.com>
2026-01-02 19:47:25 +0530

    mysql db connection

--- Day-1/sqlDbConnect/index.js
+++ Day-1/sqlDbConnect/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const db = require("./db");
 const app = express();
 require("dotenv").config();
 
@@ -12,6 +13,14 @@ app.get("/test-db", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+app.get("/users", async (req, res) => {
+  const [users] = await db.query("SELECT * FROM users");
+  res.json(users);
+});
+
+db.getConnection()
+  .then(() => console.log("DB Connected Successfully ✅"))
+  .catch((err) => console.error(err.message));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
