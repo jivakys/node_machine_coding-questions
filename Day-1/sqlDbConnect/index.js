ad842cf9df87a7ddd3828b733de3d1fb16c1684a
Jivak Sute <jivakysute@gmail.com>
2026-01-02 18:09:30 +0530

    mysql db connection

--- Day-1/sqlDbConnect/index.js
+++ Day-1/sqlDbConnect/index.js
@@ -5,5 +5,5 @@ require("dotenv").config();
 app.use(express.json());
 
 app.listen(process.env.PORT, () => {
-  console.log("Server running on port 3000");
+  console.log(`Server running on port ${process.env.PORT}`);
 });
