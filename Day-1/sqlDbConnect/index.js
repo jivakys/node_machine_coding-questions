8a4b76973a66b33aca0d16ec91ff7583bb1b1619
Jivak Sute <jivakysute@gmail.com>
2026-01-03 11:05:48 +0530

    mysql db connection

--- Day-1/sqlDbConnect/index.js
+++ Day-1/sqlDbConnect/index.js
@@ -5,14 +5,6 @@ require("dotenv").config();
 
 app.use(express.json());
 
-// app.get("/test-db", async (req, res) => {
-//   try {
-//     const [rows] = await db.query("SELECT 1");
-//     res.json({ message: "Database connected ✅" });
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// });
 app.get("/users", async (req, res) => {
   const [users] = await db.query("SELECT * FROM users");
   res.json(users);
