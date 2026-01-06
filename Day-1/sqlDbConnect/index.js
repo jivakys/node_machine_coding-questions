96b9c72e1b125c0266229cbb92b9fb68ed43cab9
Jivak Sute <jivakysute@gmail.com>
2026-01-06 19:45:36 +0530

    sql db connect

--- Day-1/sqlDbConnect/index.js
+++ Day-1/sqlDbConnect/index.js
@@ -10,6 +10,26 @@ app.get("/users", async (req, res) => {
   res.json(users);
 });
 
+app.post("/users", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const [result] = await db.query(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [name, email, password]
+    );
+    res.status(201).json({
+      message: "User created successfully",
+      userId: result.insertId,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 app.listen(process.env.PORT, async () => {
   try {
     await db.getConnection();
